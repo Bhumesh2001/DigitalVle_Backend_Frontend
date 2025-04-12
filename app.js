3ab173d0f776c3efe1b21d8d74fdccb761a60e95
bhumesh2001 <bhumesh21@navgurukul.org>
2025-04-12 18:20:16 +0530

    new code

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5000",
+    origin: "http://localhost:5001",
     credentials: true,
 }));
 app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
