6dc55a48b2cd2863aebbd5e029f6702b15fce0a9
bhumesh2001 <bhumesh21@navgurukul.org>
2025-05-13 20:38:08 +0530

    new code

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -9,7 +9,11 @@ const paymentSchema = new mongoose.Schema({
     categoryId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category',
-        required: true
+        default: null
+    },
+    isAllCategory: {
+        type: Boolean,
+        default: false
     },
     imageUrl: {
         type: String,
@@ -18,10 +22,20 @@ const paymentSchema = new mongoose.Schema({
     publicId: {
         type: String,
         required: true,
-        index: true,
+        index: true
     }
 }, {
     timestamps: true
+});
+
+// Set isAllCategory to true if categoryId is not provided
+paymentSchema.pre('save', function (next) {
+    if (!this.categoryId) {
+        this.isAllCategory = true;
+    } else {
+        this.isAllCategory = false;
+    }
+    next();
 });
 
 const Payment = mongoose.model('Payment', paymentSchema);
